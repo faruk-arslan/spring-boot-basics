c9019200d9850677b8c343bf868b240e14dfb198
nowll <farukarslan011@gmail.com>
2021-01-16 22:05:38 +0300

    Adding more Entity and Repository...

--- src/main/java/com/frdevstudio/learningspring/LearningSpringApplication.java
+++ src/main/java/com/frdevstudio/learningspring/LearningSpringApplication.java
@@ -1,7 +1,12 @@
 package com.frdevstudio.learningspring;
 
+import com.frdevstudio.learningspring.data.entity.Guest;
+import com.frdevstudio.learningspring.data.entity.Reservation;
 import com.frdevstudio.learningspring.data.entity.Room;
+import com.frdevstudio.learningspring.data.repository.GuestRepository;
+import com.frdevstudio.learningspring.data.repository.ReservationRepository;
 import com.frdevstudio.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,5 +35,25 @@ public class LearningSpringApplication {
 		public Iterable<Room> getRooms(){
 			return this.roomRepository.findAll();
 		}
+	}
+
+	@RestController
+	@RequestMapping("/guests")
+	public class GuestController{
+		@Autowired
+		private GuestRepository guestRepository;
+
+		@GetMapping
+		public Iterable<Guest> getGuests(){ return this.guestRepository.findAll();}
+	}
+
+	@RestController
+	@RequestMapping("/reservations")
+	public class ReservationController{
+		@Autowired
+		private ReservationRepository reservationRepository;
+
+		@GetMapping
+		public Iterable<Reservation> getReservations(){ return reservationRepository.findAll();}
 	}
 }
---
+++ src/main/java/com/frdevstudio/learningspring/data/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.frdevstudio.learningspring.data.repository;
+
+import com.frdevstudio.learningspring.data.entity.Reservation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends CrudRepository<Reservation, Long> {
+}
