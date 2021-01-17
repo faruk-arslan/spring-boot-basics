e8873091d40f906ff038858c3eaec0221b44489f
nowll <farukarslan011@gmail.com>
2021-01-17 22:23:27 +0300

    Adding a controller

--- src/main/java/com/frdevstudio/learningspring/LearningSpringApplication.java
+++ src/main/java/com/frdevstudio/learningspring/LearningSpringApplication.java
@@ -19,41 +19,5 @@ public class LearningSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringApplication.class, args);
 	}
-
-	// this is not a best way to do that, just for example
-	@RestController
-	@RequestMapping("/rooms")
-	public class RoomController{
-
-		private final RoomRepository roomRepository;
-
-		public RoomController(RoomRepository roomRepository) {
-			this.roomRepository = roomRepository;
-		}
-
-		@GetMapping
-		public Iterable<Room> getRooms(){
-			return this.roomRepository.findAll();
-		}
-	}
-
-	@RestController
-	@RequestMapping("/guests")
-	public class GuestController{
-		@Autowired
-		private GuestRepository guestRepository;
-
-		@GetMapping
-		public Iterable<Guest> getGuests(){ return this.guestRepository.findAll();}
-	}
-
-	@RestController
-	@RequestMapping("/reservations")
-	public class ReservationController{
-		@Autowired
-		private ReservationRepository reservationRepository;
-
-		@GetMapping
-		public Iterable<Reservation> getReservations(){ return reservationRepository.findAll();}
-	}
+	
 }
